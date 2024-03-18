ff1829332295626ceedcd4de9b1c86f99ba24555
unknown <dawid.skorpion@interia.pl>
2024-03-18 22:16:52 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# author: Dawid Piechowka
+## Project: web simple game
+### 2a SCI TI
+
+Functionalities:
+*loading screen before game
+*protection against accidental closing or overloading
+*player as img
+    *cookie accept
+    if(user.cookies.agreed){
+        CollectData(user)
+    }else{
+        CollectData(user)
+    }
+
+    whats difference
+
+    if(userInfo.cookies.agreed){
+        Collect(user.data)
+    }else{
+        Collect(user.data)
+    }
+
+    *platforms on html canvas     
+    *player's control by arrows and WASD
+    *player reacts for platforms
+    *fall animation and menu
+    *parallax platfroms looped
+    *hq player mechanics movement, move's accelerating mulitplied by time key pressed
+    *parallax background, three hues of gray stripes, rarely appears
+    *accelerating of platfroms multiplied by time 
+    *usb game controller player control
+    *every function in js has own alt text in console log as tests
+    *git 
+
+
---
+++ game.js
@@ -0,0 +1,52 @@
+window.addEventListener('beforeunload', function(event) {
+  event.preventDefault();
+  event.returnValue = '';
+   window.onload = init;
+    window.onclick = clk;
+    window.onbeforeunload = closing; //BEFOREUNLOAD 
+    
+    function closing() {
+        console.log("function alrt WORKS !!!!"); 
+        window.alert("closing now.....");
+    }
+});
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var loader = document.getElementById('loader');
+    loader.style.display = 'block';  
+    var loader_text = 0;
+    var loader_textElement = document.querySelector('.loader_text'); //LOADING TEXT
+  
+    var interval = setInterval(function() {
+        loader_text += 1;
+      if (loader_text <= 100) {
+        loader_textElement.textContent = loader_text + '%'; //LOADING CIRCLES
+      } else {
+        clearInterval(interval);
+        loader_textElement.classList.add('loaded');
+        
+        var welcomeText = document.getElementById('welcomeText');
+        welcomeText.style.display = 'block';
+        setTimeout(function() {
+          welcomeText.style.transition = 'opacity 1s'; //IamBasic DISAPPEARING
+          welcomeText.style.opacity = '0';
+        }, 3000);
+        setTimeout(function(){
+          var player = document.getElementById('player');
+          player.style.opacity = '1'; //PLAYER APPEARING
+        }, 4000);
+      }
+      setTimeout(function() {
+        loader.style.opacity = '0';
+        loader_textElement.style.display = 'none';
+    }, 6000);
+    }, 60);
+  });
+  document.addEventListener('keydown', function(event){
+    var player = document.getElementById('player');
+    var playerPosition = player.getBoundingClientRect();
+    if (event.key === 'ArrowLeft') {
+      player.style.left = (playerPosition.left - 10) + 'px'; //LEFT MOVE
+    } else if (event.key === 'ArrowRight') {
+      player.style.left = (playerPosition.left + 10) + 'px'; //RIGHT MOVE
+    }
+   });
